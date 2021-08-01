388a6d3bd5b5658f82135857fb4a077ae7220796
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-08-01 14:43:50 -0700

    add styling/positioning to header

--- src/Components/Topbar/Topbar.jsx
+++ src/Components/Topbar/Topbar.jsx
@@ -4,7 +4,13 @@ import './Topbar.scss'
 export default function Topbar() {
   return (
     <div className="topbar">
-      this
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro">intro</a>
+        </div>
+        <div className="right">
+        </div>
+      </div>
     </div>
   )
 }
