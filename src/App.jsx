04ef658ab972f901fac20968950ab9aa0a83e335
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-08-01 14:27:45 -0700

    add initial components and styling

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Topbar from "./Components/Topbar/Topbar";
+import Intro from "./Components/Intro/Intro"
+import Portfolio from "./Components/Portfolio/Portfolio"
+import Works from "./Components/Works/Works"
+import Contact from "./Components/Contact/Contact"
+import  "./app.scss"
+function App() {
+  return (
+    <div className="app">
+      <Topbar />
+        <div className="sections"> 
+        <Intro/>
+        <Portfolio />
+        <Works />
+        <Contact />
+      </div>
+    </div>
+    
+  );
+}
+
+export default App;
