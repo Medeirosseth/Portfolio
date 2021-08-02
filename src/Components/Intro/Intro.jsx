0a3df041311a98d8988e6b1c1fcf9ae57715c7fb
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-08-01 20:24:05 -0700

    add functional ityped animation for home page

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 import "./Intro.scss"
+import { init } from 'ityped'
+import { useEffect, useRef } from 'react'
 
-export default function intro() {
+export default function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+
+      strings: ["Developer", "Designer", "Musician", "Curator"]
+    })
+  }, [])
+
   return (
     <div className="intro" id="intro">
       <div className="left">
@@ -13,7 +26,7 @@ export default function intro() {
         <div className="wrapper">
           <h2>Hello World, I'm</h2>
           <h1>Seth Medeiros</h1>
-          <h3><span>Designer</span></h3>
+          <h3><span ref={textRef}>Designer</span></h3>
         </div>
         <a href="#portfolio">
           <img src="assets/DownArrow.png" alt="arrow"></img>
