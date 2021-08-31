cd79c4f8d865ba1e408f166b0698c7b4f4b07664
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-08-30 18:48:23 -0700

    update portfolio page

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -11,14 +11,21 @@ export default function Portfolio() {
       <div className="container">
         <div className="item">
           <a href="https://github.com/Medeirosseth/MovieMatcher">
-            <img src="assets/Github.png"
+            <img src="assets/epicouch.png"
             alt="github logo"></img>
             <h3>andChill</h3>
           </a>
         </div>
         <div className="item">
+          <a href="https://www.mcmonsterart.com">
+            <img src="assets/Mcmonster.png"
+            alt="github logo"></img>
+            <h3>Artist Portfolio Website</h3>
+          </a>
+        </div>
+        <div className="item">
           <a href="https://github.com/Medeirosseth/ReadyCheckGo">
-            <img src="assets/Github.png"
+            <img src="assets/ReadyCheckGo.png"
             alt="github logo"></img>
             <h3>ReadyCheckGo</h3>
           </a>
