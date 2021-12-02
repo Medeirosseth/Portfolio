242d628bd62f3aa13b90afa23a6327cb6cf18203
Seth <Medeirosseth@gamil.com>
2021-12-02 11:23:33 -0800

    add mobile view to contact page

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -18,16 +18,16 @@ export default function Contact() {
   return (
     <div className="contact" id="contact">
       <div className="left">
-      <img src="assets/Devil.jpeg" className="sethPicture" alt="Seth with Flamingos"></img>
+      <img src="assets/setherether.jpg" className="sethPicture" alt="Seth with Flamingos"></img>
       <img src="assets/Sether.png" className="picSeth" alt="Seth green background"/>
       <img src="assets/Seth.png"  className="sethPic" alt="Resume"></img>
       </div>
       <div className="right">
         <h2>Contact</h2>
         <form onSubmit={sendEmail} >
-          <input type="text" placeholder="Email" name="email" required/>
-          <input type="text" placeholder="Name" name="name" required/>
-          <textarea placeholder="Message" name="message"></textarea>
+          <input id="contactEmail" type="text" placeholder="Email" name="email" required/>
+          <input id="contactName"  type="text" placeholder="Name" name="name" required/>
+          <textarea id="contactTextArea"  placeholder="Message..." name="message"></textarea>
           <button onSubmit={sendEmail} type="submit">Send</button>
 
         </form>
